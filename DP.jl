7ad2fc5fb7499192d801ae3e4794daa49ec10d73
lab57 <luc.barrett57@gmail.com>
2024-12-09 18:36:18 -0500

    yipee

--- DP.jl
+++ DP.jl
@@ -3,15 +3,31 @@ using Plots
 using Symbolics
 using SciMLStructures:Tunable, replace, replace!
 using SymbolicIndexingInterface: parameter_values, state_values
-using ModelingToolkit: Nonnumeric
+using ModelingToolkit: Nonnumeric, structural_parameters
+
+
+
+struct state
+    x::Real
+    θ1::Real
+    θ2::Real
+    dx::Real
+    ω1::Real
+    ω2::Real
+end
+
+
+
+
+
 
 begin
 
-    function createSystem()
+    function createSystem( H_ARG::DataType)
         @independent_variables t
         # Define states and their derivatives explicitly
         @variables x(t) θ1(t) θ2(t) dx(t) dθ1(t) dθ2(t)
-        @parameters  mc m1 m2 L1 L2 g H(::Real, ::Real, ::Real)
+        @parameters  mc m1 m2 L1 L2 g H(::Real, ::Real, ::Real, ::Real, ::Real, ::Real, ::H_ARG) ARGS::H_ARG
         xs = [x, θ1, θ2] #all motion variables 
         d = Differential(t) #d/dt
         ω1 = d(θ1)
@@ -42,15 +58,15 @@ begin
         T = T1 + T2 + T3 + T4 + T5 + T6
         V = g*(m1*l1 + m2*L1)*cos(θ1) + m2*g*l2*cos(θ2)
         L = T - V
-
-        EL = expand_derivatives(d(Differential(dx)(L))) - expand_derivatives(Differential(x)(L)) ~ H(θ1, θ2, t)  # force(θ1, x, t)
+        st = state(x, θ1, θ2, dx, dθ1, dθ2)
+        EL = expand_derivatives(d(Differential(dx)(L))) - expand_derivatives(Differential(x)(L)) ~ H(x, θ1, θ2, dx, dθ1, dθ2, ARGS)  # force(θ1, x, t)
         push!(eqs, EL )
         EL = expand_derivatives(d(Differential(dθ1)(L))) - expand_derivatives(Differential(θ1)(L)) ~ 0#H(θ1, θ2, t)  # force(θ1, x, t)
         push!(eqs, EL )
         EL = expand_derivatives(d(Differential(dθ2)(L))) - expand_derivatives(Differential(θ2)(L)) ~ 0#H(θ1, θ2, t)  # force(θ1, x, t)
         push!(eqs, EL )
 
-        @named HO = ODESystem(eqs, t, [x, θ1, θ2, dx, dθ1, dθ2], [mc, m1, m2, L1, L2, g, H])
+        @named HO = ODESystem(eqs, t, [x, θ1, θ2, dx, dθ1, dθ2], [mc, m1, m2, L1, L2, g, H, ARGS])
         HO = structural_simplify(HO)
         for e in equations(HO)
             println(e)
@@ -59,14 +75,11 @@ begin
     end
 
 
-    function createProblem(sys)
+    function createProblem(sys, H, arginit)
         tspan = (0, 20) 
         # IC = [sys.x=>.2, sys.θ1=>.1, sys.θ2=>.1, sys.dx=>0, sys.dθ1=>0, sys.dθ2=>0]
         IC = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0,0.0,0.0]
-        function null(t, a, b)
-            return 0
-        end
-        ps = [sys.mc=>100, sys.m1=> 1, sys.m2 => 1, sys.L1 => 1, sys.L2=>1, sys.g=>-9.8, sys.H=>null]
+        ps = [sys.mc=>100, sys.m1=> 1, sys.m2 => 1, sys.L1 => 1, sys.L2=>1, sys.g=>-9.8, sys.H => H, sys.ARGS=>arginit]
         prob = ODEProblem(sys, IC, tspan, ps)
     
         return prob 
@@ -74,51 +87,76 @@ begin
     
     function loss(prob,x, ICs)
         ps = parameter_values(prob)
-       
-        ps = replace(Tunable(), ps, x[1:end-1])
-        ps = replace(Nonnumeric(), ps, [x[end]])
-        println("aaa")
         println(ps[1])
+        ps = replace(Tunable(), ps, x)
+        # println("aaa")
+        # println(ps[1])
         newprob = remake(prob; u0=ICs, p=ps )
         # println("a")
         # println(newprob.u0)
         sol = solve(newprob,ImplicitMidpoint(),dt=0.05)
         return sol
     end
-    
-    println("creating sys")
-    sys = createSystem()
-    println("Creating problem")
-    prob = createProblem(sys)
-    println("solving")
-    sol = solve(prob, ImplicitMidpoint(), dt=0.05) 
+
+    mutable struct force_arg 
+        a::Float64
+        b::Float64
+    end
+    function example()
+        function FORCE(x, θ1, θ2, v, ω1, ω2, ARGS)
+            return -x + θ1 + ARGS.a * ARGS.b + sin(v)
+        end
+
+
+        sys = createSystem(force_arg)
+        init_struct = force_arg(0, 0)
+        prob = createProblem(sys,FORCE, init_struct)
+        sol = solve(prob, ImplicitMidpoint(), dt=0.05) 
+        plot(sol.t, transpose(sol[1:3, :]))
+
+
+        ainit.a = 0
+        #     # m2, m1, L1, g, mc, L2
+        #     # x, theta1, theta2, omega2, omega1, v, 0, 0 (last two must be zero, dont ask)
+        @time sol = loss(prob, [1, 1, 1, -9.8, 1000,1 ,arg_example(0) ], [0.0, 0.0, 0, 0, 0, 0,0,0])
+        plot(sol.t, transpose(sol[1:3, :]))
+
+        ainit.a = 3
+        #     # m2, m1, L1, g, mc, L2
+        #     # x, theta1, theta2, omega2, omega1, v, 0, 0 (last two must be zero, dont ask)
+        @time sol = loss(prob, [1, 1, 1, -9.8, 1000,1 ,arg_example(0) ], [0.0, 0.0, 0, 0, 0, 0,0,0])
+        plot(sol.t, transpose(sol[1:3, :]))
+
+        ainit.a = -0.001
+        #     # m2, m1, L1, g, mc, L2
+        #     # x, theta1, theta2, omega2, omega1, v, 0, 0 (last two must be zero, dont ask)
+        @time sol = loss(prob, [1, 1, 1, -9.8, 1,1 ,arg_example(0) ], [0.0, 1.0, 0, 0, 0, 0,0,0])
+        plot(sol.t, transpose(sol[1:3, :]))
+    end
+    example()
+
+    # function Force(a, b, c)
+    #     return 0.01323
+    # end
+    # Force(1, 1, 1)
+    # props =  [2, 1, 1, -9.8, 50,1 ,Force ]
+    # IC = [1.23343, .5, .3, 0, 0, 0,0,0]
+    # println(ps2)
+    # println("Loss")
+    # @time sol = loss(prob, props, IC)
     # plot(sol.t, transpose(sol[1:3, :]))
 
-
-        # m2, m1, L1, g, mc, L2
-        # x, theta1, theta2, omega2, omega1, v, 0, 0 (last two must be zero, dont ask)
-    function Force(a, b, c)
-        return 0.01323
-    end
-    Force(1, 1, 1)
-    props =  [2, 1, 1, -9.8, 50,1 ,Force ]
-    IC = [1.23343, .5, .3, 0, 0, 0,0,0]
-    println(ps2)
-    println("Loss")
-    @time sol = loss(prob, props, IC)
-    plot(sol.t, transpose(sol[1:3, :]))
-
-    function Force(a, b, c)
-        return 0.232
-    end
-    Force(1, 1, 1)
-    props =  [2, 1, 1, -9.8, 50,1 ,Force ]
-    IC = [1.23343, .5, .3, 0, 0, 0,0,0]
-    println(ps2)
-    println("Loss")
-    @time sol = loss(prob, props, IC)
-    plot(sol.t, transpose(sol[1:3, :]))
-    # println(sol[3, :])
+    # function Force(a, b, c)
+    #     return 0.232
+    # end
+    # Force(1, 1, 1)
+    # props =  [2, 1, 1, -9.8, 50,1 ,1, ]
+    # IC = [1.23343, .5, .3, 0, 0, 0,0,0]
+    # println(ps2)
+    # println("Loss")
+    # @time sol = loss(prob, props, IC)
+    # plot(sol.t, transpose(sol[1:3, :]))
+    # # println(sol[3, :])
 
 
 
