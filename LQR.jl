858da72321faa92dca0eef2ecb510850c81978a1
LoickMarion <loick.marion@gmail.com>
2024-12-09 20:33:35 -0500

    d

---
+++ LQR.jl
@@ -0,0 +1,7 @@
+using ModelingToolkit, DifferentialEquations
+using Plots
+using Symbolics
+using SciMLStructures:Tunable, replace, replace!
+using SymbolicIndexingInterface: parameter_values, state_values
+using ModelingToolkit: Nonnumeric, structural_parameters
+using LinearAlgebra, ControlSystems
