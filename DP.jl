603998a78177716bb76a34b00c7ae6805dedfc08
LoickMarion <loick.marion@gmail.com>
2024-12-12 00:56:39 -0500

    code runs, doesnt balance

--- DP.jl
+++ DP.jl
@@ -12,7 +12,7 @@ begin
         # Define states and their derivatives explicitly
         @variables x(t) θ1(t) θ2(t) dx(t) dθ1(t) dθ2(t)
         @parameters  mc m1 m2 L1 L2 g H(::Real, ::Real, ::Real, ::Real, ::Real, ::Real, ::H_ARG) ARGS::H_ARG
-        d = Differential(t); #d/dt
+        d = Differential(t) #d/dt
 
 
         # Define first-order equations
@@ -27,142 +27,70 @@ begin
         l2 = L2
         
         # Use dx, dθ1, dθ2 instead of ẋ, ω1, ω2
-        T1 = 1/2 * (mc + m1 + m2) * dx^2 ;
-        T2 = 1/2 * (m1*l1^2 + m2*L1^2 + I1) * dθ1^2 ;
-        T3 = 1/2 * (m2*l2^2 + I2) * dθ2^2  ;
+        T1 = 1/2 * (mc + m1 + m2) * dx^2 
+        T2 = 1/2 * (m1*l1^2 + m2*L1^2 + I1) * dθ1^2 
+        T3 = 1/2 * (m2*l2^2 + I2) * dθ2^2 
         
-        T4 = (m1*l1 + m2*L1) * cos(θ1) * dx * dθ1 ;
-        T5 = m2*l2 * cos(θ2) * dx * dθ2 ;
-        T6 = m2*L1*l2 * cos(θ1 - θ2) * dθ1 * dθ2 ;
-        
-        T = T1 + T2 + T3 + T4 + T5 + T6
-        V = g*(m1*l1 + m2*L1)*cos(θ1) + m2*g*l2*cos(θ2)
-        L = T - V
-        EL = expand_derivatives(d(Differential(dx)(L))) - expand_derivatives(Differential(x)(L)) ~ H(x, θ1, θ2, dx, dθ1, dθ2, ARGS);  # force(θ1, x, t)
-        push!(eqs, EL );
-        EL = expand_derivatives(d(Differential(dθ1)(L))) - expand_derivatives(Differential(θ1)(L)) ~ 0;#H(θ1, θ2, t)  # force(θ1, x, t)
-        push!(eqs, EL );
-        EL = expand_derivatives(d(Differential(dθ2)(L))) - expand_derivatives(Differential(θ2)(L)) ~ 0;#H(θ1, θ2, t)  # force(θ1, x, t)
-        push!(eqs, EL );
-
-        @named HO = ODESystem(eqs, t, [x, θ1, θ2, dx, dθ1, dθ2], [mc, m1, m2, L1, L2, g, H, ARGS]);
-        HO = structural_simplify(HO);
-        for e in equations(HO);
-            println(e);
-        end
-        return HO;
-    end
-
-    function createSystemNoExt( H_ARG::DataType)
-        @independent_variables t
-        # Define states and their derivatives explicitly
-        @variables x(t) θ1(t) θ2(t) dx(t) dθ1(t) dθ2(t)
-        @parameters  mc m1 m2 L1 L2 g H(::Real, ::Real, ::Real, ::Real, ::Real, ::Real, ::H_ARG) ARGS::H_ARG
-        d = Differential(t); #d/dt
-
-
-        # Define first-order equations
-        eqs = [
-            dx ~ d(x),
-            dθ1 ~ d(θ1),
-            dθ2 ~ d(θ2),
-        ]
-        I1 = m1*L1^2
-        I2 = m2*L2^2
-        l1 = L1
-        l2 = L2
-        
-        # Use dx, dθ1, dθ2 instead of ẋ, ω1, ω2
-        T1 = 1/2 * (mc + m1 + m2) * dx^2 ;
-        T2 = 1/2 * (m1*l1^2 + m2*L1^2 + I1) * dθ1^2 ;
-        T3 = 1/2 * (m2*l2^2 + I2) * dθ2^2  ;
-        
-        T4 = (m1*l1 + m2*L1) * cos(θ1) * dx * dθ1 ;
-        T5 = m2*l2 * cos(θ2) * dx * dθ2 ;
-        T6 = m2*L1*l2 * cos(θ1 - θ2) * dθ1 * dθ2 ;
+        T4 = (m1*l1 + m2*L1) * cos(θ1) * dx * dθ1 
+        T5 = m2*l2 * cos(θ2) * dx * dθ2 
+        T6 = m2*L1*l2 * cos(θ1 - θ2) * dθ1 * dθ2 
         
         T = T1 + T2 + T3 + T4 + T5 + T6
         V = g*(m1*l1 + m2*L1)*cos(θ1) + m2*g*l2*cos(θ2)
         L = T - V
-        EL = expand_derivatives(d(Differential(dx)(L))) - expand_derivatives(Differential(x)(L)) ~ 0;  # force(θ1, x, t)
-        push!(eqs, EL );
-        EL = expand_derivatives(d(Differential(dθ1)(L))) - expand_derivatives(Differential(θ1)(L)) ~ 0;#H(θ1, θ2, t)  # force(θ1, x, t)
-        push!(eqs, EL );
-        EL = expand_derivatives(d(Differential(dθ2)(L))) - expand_derivatives(Differential(θ2)(L)) ~ 0;#H(θ1, θ2, t)  # force(θ1, x, t)
-        push!(eqs, EL );
+        EL = expand_derivatives(d(Differential(dx)(L))) - expand_derivatives(Differential(x)(L)) ~ H(x, θ1, θ2, dx, dθ1, dθ2, ARGS)  # force(θ1, x, t)
+        push!(eqs, EL )
+        EL = expand_derivatives(d(Differential(dθ1)(L))) - expand_derivatives(Differential(θ1)(L)) ~ 0#H(θ1, θ2, t)  # force(θ1, x, t)
+        push!(eqs, EL )
+        EL = expand_derivatives(d(Differential(dθ2)(L))) - expand_derivatives(Differential(θ2)(L)) ~ 0#H(θ1, θ2, t)  # force(θ1, x, t)
+        push!(eqs, EL )
 
-        @named HO = ODESystem(eqs, t, [x, θ1, θ2, dx, dθ1, dθ2], [mc, m1, m2, L1, L2, g, H, ARGS]);
-        HO = structural_simplify(HO);
-        for e in equations(HO);
-            println(e);
+        @named HO = ODESystem(eqs, t, [x, θ1, θ2, dx, dθ1, dθ2], [mc, m1, m2, L1, L2, g, H, ARGS])
+        HO = structural_simplify(HO)
+        for e in equations(HO)
+            # println(e)
         end
-        return HO;
+        return HO
     end
 
-    function createProblem(sys, H, arginit, IC=nothing, tspan = (0,20))
-        # If no initial conditions provided, use defaults
-        if IC === nothing
-            IC = [sys.x=>0.0, sys.θ1=>0.0, sys.θ2=>0.0, sys.dx=>0.0, sys.dθ1=>0.0, sys.dθ2=>0.0]
-        end
-        
-        # Check if IC is array-like or dictionary-like
-        if IC isa AbstractArray
-            # Convert array to dictionary of pairs
-            IC = [
-                sys.x => IC[1],
-                sys.θ1 => IC[2],
-                sys.θ2 => IC[3],
-                sys.dx => IC[4],
-                sys.dθ1 => IC[5],
-                sys.dθ2 => IC[6]
-            ]
-        end
-        
-        # System parameters
-        ps = [
-            sys.mc => 100,
-            sys.m1 => 1,
-            sys.m2 => 1,
-            sys.L1 => 1,
-            sys.L2 => 1,
-            sys.g => -9.8,
-            sys.H => H,
-            sys.ARGS => arginit
-        ]
-        
-        # Create and return the problem
+
+    function createProblem(sys, H, arginit, tspan = (0,20))
+        # IC = [sys.x=>.2, sys.θ1=>.1, sys.θ2=>.1, sys.dx=>0, sys.dθ1=>0, sys.dθ2=>0]
+        IC = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0,0.0,0.0]
+        ps = [sys.mc=>100, sys.m1=> 1, sys.m2 => 1, sys.L1 => 1, sys.L2=>1, sys.g=>-9.8, sys.H => H, sys.ARGS=>arginit]
         prob = ODEProblem(sys, IC, tspan, ps)
-        return prob
+    
+        return prob 
     end
     
     function loss(prob,x, ICs)
-        ps = parameter_values(prob);
+        ps = parameter_values(prob)
         # println(ps[1])
-        ps = replace(Tunable(), ps, x);
-        newprob = remake(prob; u0=ICs, p=ps );
-        sol = solve(newprob,ImplicitMidpoint(),dt=0.01);
-        return sol;
+        ps = replace(Tunable(), ps, x)
+        newprob = remake(prob; u0=ICs, p=ps )
+        sol = solve(newprob,ImplicitMidpoint(),dt=0.01)
+        return sol
     end
 
-    mutable struct force_arg_temp
-        a::Float64
-        b::Float64
+    mutable struct force_arg_temp3
+        a::Real
     end
+
     function example()
         
         function FORCE(x, θ1, θ2, v, ω1, ω2, ARGS)
             # println("Called")
-            return 0
+            return ARGS.a
         end
 
-        sys = createSystem(force_arg_temp)
-        init_struct = force_arg_temp(0, 0)
+        sys = createSystem(force_arg_temp3)
+        init_struct = force_arg_temp3(0)
         prob = createProblem(sys,FORCE, init_struct)
 
         ICs = [0, π/2, π, 0, 0, 0, 0, 0]
 
 
-        println("Initial Conditions: $ICs")
+        # println("Initial Conditions: $ICs")
         params = [1, 1, 1, -9.8, 1, 1]
 
         # Update Problem Parameters
@@ -176,6 +104,7 @@ begin
 
         times = Float64[]
         states = Vector{Vector{Float64}}()
+        jacobians = []
 
         t_curr = 0.0
         max_t = 20.0
@@ -184,7 +113,7 @@ begin
             push!(times, t_curr)
             push!(states, state)
 
-
+            force_arg_temp3.a = 5.0
             step!(integrator, dt, true)
             
             t_curr += dt
@@ -194,5 +123,5 @@ begin
         plot(times, states_matrix[1:3, :]')
 
     end
-    example()
+    # example()
 end
--- LQR.jl
+++ LQR.jl
@@ -5,7 +5,12 @@ using SciMLStructures:Tunable, replace, replace!
 using SymbolicIndexingInterface: parameter_values, state_values
 using ModelingToolkit: Nonnumeric
 using LinearAlgebra, ControlSystems
-include("./DP.jl");
+
+open("/dev/null", "w") do io
+    redirect_stdout(io) do
+        include("./DP.jl")
+    end
+end
 
 begin
 
@@ -14,13 +19,14 @@ begin
 
     # Function to calculate the Jacobian matrix
     function calculate_symbolic_jacobian(sys)
-        J = expand_derivatives.(calculate_jacobian(sys));
-        return J;
+        j = expand_derivatives.(calculate_jacobian(sys));
+        return j;
     end
      
-    function evaluate_jacobian(sys, inputs)
+    function evaluate_jacobian(j, sys, inputs)
         j = ModelingToolkit.expand_derivatives.(calculate_jacobian(sys))
         states = unknowns(sys)
+        # print(states)
         params = parameters(sys)
         st_d = Dict(
             [
@@ -32,6 +38,8 @@ begin
             sys.dθ2 => inputs[6],
             unknowns(sys)[7] => inputs[7],
             unknowns(sys)[8]=> inputs[8],
+            # unknowns(sys)[7] => 1,
+            # unknowns(sys)[8]=> 1,
             sys.m1 => 1,
             sys.m2 => 1,
             sys.mc => 5,
@@ -76,7 +84,7 @@ begin
     
 
 
-    function calculateK(inputs, Q, R)
+    function calculateK(inputs, j, Q, R)
             
         function FORCE(x, θ1, θ2, v, ω1, ω2, ARGS)
             return 0
@@ -84,18 +92,19 @@ begin
 
         sys = createSystemNoExt(force_arg_temp);
         init_struct = force_arg_temp(0, 0);
-        state_variables = inputs[1:6]
+        state_variables = inputs[1:6] 
 
         prob = createProblem(sys,FORCE, init_struct, state_variables, (0,.1));
 
         # Evaluate the Jacobian
-        A_evaluated = Float64.((evaluate_jacobian(sys,inputs))[1:6,1:6])
+
+
+        A_evaluated = Float64.((evaluate_jacobian(j,sys,inputs))[[1:3; 6:end], [1:3; 6:end]])
+        # A_evaluated = Float64.((evaluate_jacobian(j,sys,inputs))[1:6, 1:6]);
         B = Float64.(calculate_parametric_jacobian());
 
         L = lqr(Discrete,A_evaluated,B,Q,R) 
         return L;
-
-
     end
 
     function setup_system(initial_state, dt)
@@ -108,15 +117,15 @@ begin
         # prob = createProblem(sys, FORCE, init_struct)
         
 
-        sys = createSystem(force_arg_temp)
-        init_struct = force_arg_temp(0, 0)
+        sys = createSystem(force_arg_temp3)
+        init_struct = force_arg_temp3(0)
         prob = createProblem(sys,FORCE, init_struct)
 
 
         # Add the two additional state variables as in your example
-        ICs = [initial_state..., 0, 0]
+        ICs = [initial_state[1:6]..., 0, 0]
         
-        params = [1, 1, 1, -9.8, 1, 1]  # Your standard parameters
+        params = [1, 1, 5, -9.8, 1, 1]  # Your standard parameters
         
         # Update Problem Parameters using your method
         ps = parameter_values(prob)
@@ -124,26 +133,32 @@ begin
         newprob = remake(prob; u0=ICs, p=ps)
         
         integrator = init(newprob, ImplicitMidpoint(), dt=dt)
-        return integrator, init_struct
+
+        j = calculate_symbolic_jacobian(sys)
+            
+        return integrator, init_struct, j
     end
     
-    function step_system(integrator, init_struct, current_state, Q, R, dt)
+    function step_system(integrator, init_struct, current_state, j, Q, R, dt)
+        xref = [0.0, π, π, 0.0, 0.0, 0.0]
+    
         # Recalculate K for current state
-        K = calculateK(current_state, Q, R)
+        K = calculateK(current_state, j, Q, R)
         
-        # Calculate control input
-        u = -K * current_state
+        # Calculate control input relative to reference state
+        u = -K * (current_state[1:6] - xref)
+
         init_struct.a = u[1]  # Update force in init_struct
         
         # Step forward
         step!(integrator, dt, true)
         
-        # Return new state (first 6 components as in your example)
-        return copy(integrator.u[1:6])
+        # Return new state (all 8 components as in your example)
+        return copy(integrator.u)
     end
     
-    function run_until_complete(initial_state, Q, R, dt; max_t=20.0, max_x=10.0)
-        integrator, init_struct = setup_system(initial_state, dt)
+    function run_until_complete(initial_state, Q, R, dt; max_t=4.0, max_x=10.0)
+        integrator, init_struct, j = setup_system(initial_state, dt)
         
         times = Float64[]
         states = Vector{Vector{Float64}}()
@@ -154,7 +169,7 @@ begin
         
         while t_curr <= max_t
             push!(times, t_curr)
-            push!(states, current_state)
+            push!(states, current_state[1:6])
             
             # Check termination
             x = current_state[1]
@@ -163,8 +178,11 @@ begin
             end
             
             # Step forward
-            current_state = step_system(integrator, init_struct, current_state, Q, R, dt)
+            current_state = step_system(integrator, init_struct, current_state, j, Q, R, dt)
             t_curr += dt
+            println(t_curr);
+            println();
+
             
             # Store control input
             push!(controls, init_struct.a)
@@ -174,9 +192,71 @@ begin
     end
 
 
-    initial_state = [1.0,1.0,1.0,1.0,1.0,1.0]
-    Q = Diagonal([10.0, 1.0, 100.0, 1.0, 100.0, 1.0])
-    R = [1.0]
-    times, states = run_until_complete(initial_state, Q, R, 0.01)
+    function animate_pendulum(times, states; fps=30)
+        # Animation settings
+        L1 = 1.0  # Length of first pendulum
+        L2 = 1.0  # Length of second pendulum
+        
+        # Create animation
+        anim = @animate for i in 1:length(times)
+            # Get current state
+            x = states[i][1]     # cart position
+            θ1 = states[i][2]    # first pendulum angle
+            θ2 = states[i][3]    # second pendulum angle
+            
+            # Calculate positions
+            # Cart
+            cart_x = x
+            cart_y = 0
+            
+            # First pendulum end position
+            p1_x = cart_x + L1 * sin(θ1)
+            p1_y = -L1 * cos(θ1)
+            
+            # Second pendulum end position
+            p2_x = p1_x + L2 * sin(θ2)
+            p2_y = p1_y - L2 * cos(θ2)
+            
+            # Plot
+            plt = plot(
+                xlim = (-5, 5),
+                ylim = (-1, 3),
+                aspect_ratio = :equal,
+                legend = false,
+                title = "t = $(round(times[i], digits=2))s"
+            )
+            
+            # Draw cart
+            plot!([cart_x-0.2, cart_x+0.2], [cart_y, cart_y], color=:blue, linewidth=5)
+            
+            # Draw pendulums
+            plot!([cart_x, p1_x], [cart_y, p1_y], color=:black, linewidth=2)
+            plot!([p1_x, p2_x], [p1_y, p2_y], color=:black, linewidth=2)
+            
+            # Draw masses
+            scatter!([p1_x], [p1_y], color=:red, markersize=10)
+            scatter!([p2_x], [p2_y], color=:green, markersize=10)
+        end
+        
+        return gif(anim, "pendulum.gif", fps=fps)
+    end
 
+    initial_state = [0.1, 3, 3, 0.0, 0.0, 0.0,0,0]
+    Q = Float64.(Diagonal([10.0, 100.0, 100.0, 1.0, 1.0, 1.0]))
+    R = I
+    times, states, controls = run_until_complete(initial_state, Q, R, 0.001)
+
+    animation = animate_pendulum(times,states)
+
+    # Create a plot with multiple subplots
+    p1 = plot(times, getindex.(states, 1), label="x", title="Cart Position")
+    p2 = plot(times, getindex.(states, 2), label="θ1", title="Pendulum 1 Angle")
+    p3 = plot(times, getindex.(states, 3), label="θ2", title="Pendulum 2 Angle")
+    p4 = plot(times, controls, label="Control Force", title="Control Input")
+
+    # Combine plots into a single figure
+    combined_plot = plot(p1, p2, p3, p4, layout=(4,1), size=(800,800))
+
+    # Display or save the plot
+    savefig(combined_plot, "system_states_plot.png")
 end
